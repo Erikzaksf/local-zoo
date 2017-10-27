37a0f05b173151a92f982b84b6adee897d8ac0e7
erikzaksf <erik.zak.sf@gmail.com>
2017-10-27 10:08:09 -0700

    add initial files and folders for node and angular with working build and serve

---
+++ app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class= "jumbotron">
+  <h1>Seattle Zoo Caretaker Manager App</h1>
+  </div>
+  <div class= "container">
+  <h2> This utility is available to to the Seattle Zoo in an effort to maximize Caretaker efficiency and staffing.</h2>
+  </div>
+
+
+  `
+})
+
+
+export class AppComponent {
+
+}
