80d45d68283d9ef1d862e834d443f9db84c7554c
erikzaksf <erik.zak.sf@gmail.com>
2017-10-27 10:40:00 -0700

    add first hard coded animal and display it in browser

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,11 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <div class= "jumbotron">
+  <div class= "jumbotron" style="text-align: center;">
   <h1>Seattle Zoo Caretaker Manager App</h1>
+  <h3>{{month}}/{{day}}/{{year}}</h3>
+  <hr>
+  <h2>{{statement}}</h2>
   </div>
   <div class= "container">
   <h2> This utility is available to to the Seattle Zoo in an effort to maximize Caretaker efficiency and staffing.</h2>
+
+  <div class="well">
+    <ul>
+      <li style="font-size: 30px;">         {{animalInfo.species}}</li>
+      <li>Name:     {{animalInfo.name}}</li>
+      <li>Age:      {{animalInfo.age}}</li>
+      <li>Diet:     {{animalInfo.diet}}</li>
+      <li>location:    {{animalInfo.location}}</li>
+      <li>Caretakers: {{animalInfo.caretakers}}</li>
+      <li>Sex:       {{animalInfo.sex}}</li>
+      <li>Likes:       {{animalInfo.likes}}</li>
+      <li>Dislikes:         {{animalInfo.dislikes}}</li>
+    </ul>
+  </div>
+
   </div>
 
 
@@ -17,4 +35,20 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 
+  statement: string = 'Leadership is our mission statement.';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  animalInfo = {
+    species: "Arctic Fox",
+    name: "Moon",
+    age: 2,
+    diet: "Carnivore",
+    location: "Northern Trail",
+    caretakers: 5,
+    sex: "Female",
+    likes: "Cool shade",
+    dislikes: "Loud noises",
+  }
 }
